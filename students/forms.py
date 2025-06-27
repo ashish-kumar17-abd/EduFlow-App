dc5a2e43b62163818b15c93b72c810142f4a92b0
Ashish <ashishkumarsvrp1@gmail.com>
2025-06-27 14:31:28 +0530

    💄 Polished student dashboard UI and improved timetable page

--- students/forms.py
+++ students/forms.py
@@ -5,3 +5,10 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         exclude = ['user']  # user ko auto set karenge views me
+
+        widgets = {
+            'full_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'dob': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'gender': forms.Select(attrs={'class': 'form-control'}),
+            'profile_picture': forms.FileInput(attrs={'class': 'form-control'}),
+        }
